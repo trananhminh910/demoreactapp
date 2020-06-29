4c9798df88e46af61059253fdc102c4ff2096145
Tran Anh Minh <trananhminh910@gmail.com>
2020-06-29 21:57:17 +0700

    Updates

---
+++ precache-manifest.02e2ac8d09db9a370d9d6126b2747e16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "854116c7de454b92c408a007105cc435",
+    "url": "/demoreactapp/index.html"
+  },
+  {
+    "revision": "cc4f31ccab037e944010",
+    "url": "/demoreactapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "048370006d009c9b22ff",
+    "url": "/demoreactapp/static/js/2.cee53a46.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/demoreactapp/static/js/2.cee53a46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc4f31ccab037e944010",
+    "url": "/demoreactapp/static/js/main.f3eb98f5.chunk.js"
+  },
+  {
+    "revision": "35ebf8dff70778a058c0",
+    "url": "/demoreactapp/static/js/runtime-main.93e8053b.js"
+  }
+]);
